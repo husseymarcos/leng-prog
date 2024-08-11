35e7d0282b2d6526876846fefa8495dbd22891f8
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-11 16:23:15 -0300

    collatz + fraction

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,22 @@
 module CollatzConjecture (collatz) where
 
+-- First iteration (pure function)
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n = if n <=0 then Nothing else collatzsWithSteps n 0
+    where
+        collatzsWithSteps 1 steps = Just steps
+        collatzsWithSteps a steps = 
+            if even a then collatzsWithSteps (a `div` 2) (steps + 1)
+            else collatzsWithSteps ((3 * a) + 1) (steps + 1) 
+
+
+-- Second iteration (with aux function)
+-- collatz :: Integer -> Maybe Integer
+-- collatz n = if n <=0 then Nothing else collatzWithSteps n 0
+
+
+-- collatzWithSteps :: Integer -> Integer -> Maybe Integer
+-- collatzWithSteps 1 steps = Just steps
+-- collatzWithSteps a steps = 
+--     if even a then collatzWithSteps (a `div` 2) (steps + 1)
+--     else collatzWithSteps ((3 * a) + 1) (steps + 1) 
--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -5,27 +5,40 @@ type Fraction = (Int, Int)
 -- Implement the `add` Function
 
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add n d = (n1 * d2 + d1 * n2, n2 * d2)
+  where
+      (n1, n2) = n
+      (d1, d2) = d
 
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub n d = (n1 * d2 - d1 * n2, n2 * d2)
+  where
+    (n1, n2) = n
+    (d1, d2) = d
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul n d = (n1 * d1, n2 * d2)
+  where
+      (n1, n2) = n
+      (d1, d2) = d
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide n d = (n1 * d2, n2 * d1)
+  where
+      (n1, n2) = n
+      (d1, d2) = d
 
 -- Implement the `hcf` Function
 
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
-
-
-    
+hcf a b
+  | a == b = a
+  | a > b = hcf (a - b) b
+  | otherwise = hcf a (b - a)
+  
