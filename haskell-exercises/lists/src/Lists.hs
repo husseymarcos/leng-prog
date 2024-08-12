067ff82b7e8b4d2657dfc7a2345c66f0dc41bc78
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-12 19:37:38 -0300

    binaryToDecimal

--- haskell-exercises/lists/src/Lists.hs
+++ haskell-exercises/lists/src/Lists.hs
@@ -16,36 +16,11 @@ union (x:xs) ys
   | member x ys = union xs ys
   | otherwise   = x : union xs ys
 
--- intersection:: [Int] -> [Int] -> [Int]
--- -- Me fijo si una lista esta vacia devuelvo otra lista vacia
--- intersection [] _ = []
--- -- Agarro un elemento de la primera lista
--- intersection (x:xs) ys =
---   -- Si es miembro de la segunda lista lo concateno con la interseccion del resto de la primera lista con la segunda
---   if member x ys then x : intersection xs ys
---   -- Sino sigo con el resto de la lista
---   else intersection xs ys
-
 intersection:: [Int] -> [Int] -> [Int]
 intersection [] _ = []
 intersection (x:xs) ys =
   if member x ys then x : intersection xs ys
   else intersection xs ys
-
-
--- difference:: [Int] -> [Int] -> [Int]
--- -- Si la segunda lista esta vacia devuelvo la primera
--- difference xs [] = a
--- difference [] ys = []
--- -- Si las listas son iguales devolver una lista vacia 
--- difference _ _ = []
--- -- Agarro un elemento de y y me fijo si pertenece a 
--- difference (x:xs) ys = 
---   -- Si el elemento de la segunda lista es parte de la primera entonces seguir
---   if member x ys then difference xs ys
---   -- Si no es parte entonces agregar x a la lista y seguir con 
---   else  x : difference xs ys
-
 
 difference :: [Int] -> [Int] -> [Int]
 difference [] _ = []
@@ -59,20 +34,20 @@ insert:: Int -> [Int] -> [Int]
 insert x [] = [x]
 insert x (y:ys) =
   if x >= y then y : insert x ys
-  else  x : y : ys
+  else  x : (y:ys)
 
-
--- Usando foldr
 insertionSort :: [Int] -> [Int]
 insertionSort = foldr insert []
 
--- insertionSort :: [Int] -> [Int]
--- insertionSort [] = []
--- insertionSort (x:xs) = insert x (insertionSort xs)
-
 binaryToDecimal :: [Int] -> Int
-binaryToDecimal = error "Implement it"
+binaryToDecimal [] = 0
+binaryToDecimal (x:xs) = x * 2 ^ length xs + binaryToDecimal xs
 
+
+reverseList :: [Int] -> [Int]
+reverseList [] = []
+reverseList (x:xs) =
+  reverseList xs ++ [x]
 
 toDecimal :: Int -> [Int] -> Int
 toDecimal = error "Implement it"
